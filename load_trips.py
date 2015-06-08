82a5bee2269a784a60af950d2acf8b99591813a4
Nils Breyer <nbnet@arcor.de>
2015-06-08 11:42:34 -0700

    fix trip loading

--- load_trips.py
+++ load_trips.py
@@ -11,14 +11,14 @@ def read_trip(args):
 	Args:
 		args: a tuple (i, line), where i is the line number and line the a one-line string from the trip csv
 	Returns:
-		A list [((user_id, cellpath),1)] where cellpath is a ordered list of cell ids visited on the trip"""
+		A list [(user_id, cellpath))] where cellpath is a ordered list of cell ids visited on the trip"""
 
 	i, line = args
 
 	t = util.parse_trip(line)
 	if not t == None:
 		user_id, cellpath = t 
-		return [((user_id, cellpath),1)]
+		return [(user_id, cellpath)]
 	else:
 		return []
 
@@ -29,11 +29,10 @@ def upload_trip(args):
 		are the attributes of the trip
 	"""
 
-	key, values = args
-	print("test")
+	user_id, cellpathlist = args
+	cellpath = cellpathlist[0]
 	conn = util.db_connect()
 	cur = conn.cursor()
-	user_id, cellpath = key
 	sql = "	INSERT INTO trips (user_id, start_antenna, end_antenna, cellpath) \
 			VALUES (%s, %s, %s, %s);"
 	cur.execute(sql, (user_id, cellpath[0], cellpath[-1], cellpath))
