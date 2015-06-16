7770ba751220f15e0c8284ff6470fa27d722bd5c
Nils Breyer <nbnet@arcor.de>
2015-06-16 13:44:04 -0700

    backup only trips and antennas

--- backup_original.py
+++ backup_original.py
@@ -19,17 +19,3 @@ cur.execute("DROP TABLE IF EXISTS trips_original CASCADE")
 conn.commit()
 cur.execute("CREATE TABLE trips_original AS SELECT * FROM trips;")
 conn.commit()
-
-#create backup copy taz_original before joining TAZs
-print("Creating backup table taz_original (takes a while)...")
-cur.execute("DROP TABLE IF EXISTS taz_original CASCADE")
-conn.commit()
-cur.execute("CREATE TABLE taz_original AS SELECT * FROM taz;")
-conn.commit()
-
-#create backup copy od_original before joining TAZs
-print("Creating backup table od_original (takes a while)...")
-cur.execute("DROP TABLE IF EXISTS od_original CASCADE")
-conn.commit()
-cur.execute("CREATE TABLE od_original AS SELECT * FROM od;")
-conn.commit()
