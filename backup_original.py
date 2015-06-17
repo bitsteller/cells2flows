2ae0153b463e0fb0729e387c2b55d8a70ba4d874
Nils Breyer <nbnet@arcor.de>
2015-06-16 18:58:13 -0700

    create backup tables with primary key

--- backup_original.py
+++ backup_original.py
@@ -10,12 +10,18 @@ cur = conn.cursor()
 print("Creating backup table ant_pos_original (takes a while)...")
 cur.execute("DROP TABLE IF EXISTS ant_pos_original CASCADE")
 conn.commit()
-cur.execute("CREATE TABLE ant_pos_original AS SELECT * FROM ant_pos;")
+cur.execute("CREATE TABLE ant_pos_original (LIKE ant_pos);")
+cur.execute("ALTER TABLE ant_pos_original ADD CONSTRAINT ant_pos_original_pkey PRIMARY KEY (id);")
+cur.execute("INSERT INTO ant_pos_original SELECT * FROM ant_pos;")
 conn.commit()
+
+
 
 #create backup copy trips_original before clustering
 print("Creating backup table trips_orignal (takes a while)...")
 cur.execute("DROP TABLE IF EXISTS trips_original CASCADE")
 conn.commit()
-cur.execute("CREATE TABLE trips_original AS SELECT * FROM trips;")
+cur.execute("CREATE TABLE trips_original (LIKE trips);")
+cur.execute("ALTER TABLE trips_original ADD CONSTRAINT trips_original_pkey PRIMARY KEY (id);")
+cur.execute("INSERT INTO trips_original SELECT * FROM trips;")
 conn.commit()
