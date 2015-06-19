979344c790f63b4f2767b76a73cf60c7bdd4c581
Nils Breyer <nbnet@arcor.de>
2015-06-18 17:32:28 -0700

    implement convert_od

---
+++ SQL/01a_Preprocessing/create_get_cells_for_taz_func.sql
@@ -0,0 +1,29 @@
+-- get_cells_for_taz(taz_id) searches for all cells that intersect with the taz
+-- returns a table containing all intersecting cells and their share of the area of the TAZ that they cover
+-- the sum of all shares adds up to 1.0
+
+CREATE OR REPLACE FUNCTION get_cells_for_taz(integer) RETURNS TABLE(cellid numeric(10,0), share double precision) AS $$
+BEGIN
+  RETURN QUERY 
+  WITH taz AS (SELECT *, ST_AREA(taz.geom) AS area FROM taz WHERE taz_id = $1),
+     cells AS (SELECT voronoi.id AS id, voronoi.geom AS geom FROM voronoi, taz WHERE ST_Intersects(voronoi.geom, taz.geom))
+  SELECT cell.id AS cellid, ST_AREA(ST_INTERSECTION(taz.geom, cell.geom))/taz.area AS share
+  FROM cells AS cell, taz;
+  RETURN;
+END
+$$ LANGUAGE plpgsql STABLE;
+
+
+--cache cell mapping in materialized view for performance
+DROP MATERIALIZED VIEW IF EXISTS taz_cells CASCADE;
+
+CREATE MATERIALIZED VIEW taz_cells AS 
+ SELECT taz.taz_id,
+    get_cells_for_taz.cellid AS cell_id,
+    get_cells_for_taz.share
+   FROM taz,
+    LATERAL get_cells_for_taz(taz.taz_id) get_cells_for_taz(cellid, share)
+WITH DATA;
+
+
+--SELECT get_cells_for_taz(22062000)
--- convert_od.py
+++ convert_od.py
@@ -1,30 +1,64 @@
-import psycopg2, signal, sys
+import psycopg2, signal, sys, time
 
 import util, config #local modules
 
-def fetch_taz_od():
+CHUNKSIZE = 100
+
+def fetch_taz_od_chunks():
 	conn = util.db_connect()
 	cur = conn.cursor()
-	cur.execute("SELECT orig_taz, dest_taz, flow  \
+	cur.execute("SELECT origin_taz, destination_taz, flow  \
 			 FROM taz_od \
 			 WHERE ((interval IS NULL AND %(interval)s IS NULL) OR interval = %(interval)s)",
 			 {"interval": interval})
 
+	batch = []
 	for o_taz, d_taz, flow in cur:
-		yield (o_taz, d_taz, flow)
+		batch.append((o_taz, d_taz, flow))
+		if len(batch) == CHUNKSIZE:
+			yield batch
+			batch = []
+	yield batch
 
-def calculate_cell_od_flow(args):
+def calculate_cell_od_flow(taz_od_chunk):
 	global interval
-	o_taz, d_taz, flow = args
 
-	#distribute flow on cells by area
-	#TODO
+	conn = util.db_connect()
+	cur = conn.cursor()
+	result = []
+
+	for o_taz, d_taz, flow in taz_od_chunk:
+		#fetch origina/destination cell coverage
+		cur.execute("SELECT taz_id, cell_id, share FROM taz_cells WHERE taz_id = %s OR taz_id = %s", (o_taz,d_taz))
+		o_cells = []
+		d_cells = []
+		for taz_id, cell_id, share in cur.fetchall():
+			if taz_id == o_taz:
+				o_cells.append((cell_id, share))
+			if taz_id == d_taz:
+				d_cells.append((cell_id, share))
+
+		#distribute flow on cells by area
+		od_pairs = []
+		shares = []
+		for o_cell, o_share in o_cells:
+			for d_cell, d_share in d_cells:
+				if o_share * d_share > 0.1:
+					od_pairs.append((o_cell, d_cell))
+					shares.append(o_share * d_share)
+
+		normalized_shares = [share/sum(shares) for share in shares]
+		result.extend([((o_cell, d_cell), share * flow) for (o_cell, d_cell), share in zip(od_pairs, normalized_shares)])
+	return result
 
 def upload_cell_od_flow(args):
 	global interval
 	key, flows = args
 	o_cell, d_cell = key
 	flow = sum(flows)
+
+	conn = util.db_connect()
+	cur = conn.cursor()
 	
 	data = {"orig_cell": o_cell,
 			"dest_cell": d_cell,
@@ -32,7 +66,7 @@ def upload_cell_od_flow(args):
 			"flow": flow}
 
 	cur.execute(open("SQL/01a_Preprocessing/add_cell_od_flow.sql", 'r').read(), data)
-	conn.execute()
+	conn.commit()
 
 def signal_handler(signal, frame):
 	global mapper, request_stop
@@ -55,12 +89,15 @@ if __name__ == '__main__':
 	cur.execute(open("SQL/01a_Preprocessing/create_od.sql", 'r').read())
 	conn.commit()
 
+	print("Creating taz_cell view mapping TAZ to cells...")
+	cur.execute(open("SQL/01a_Preprocessing/create_get_cells_for_taz_func.sql", 'r').read())
+	conn.commit()
 
 	#fetch different interval values
 	cur.execute("SELECT array_agg(DISTINCT interval) FROM taz_od")
 	intervals = cur.fetchone()[0]
 	for i, interval in enumerate(intervals):
-		print("Converting OD flows to cellflows for interval " + str(interval) + " (" + str(i+1) + "/" + str(len(intervals)) + ")...")
+		print("Converting TAZ OD flows to cell OD flows for interval " + str(interval) + " (" + str(i+1) + "/" + str(len(intervals)) + ")...")
 
 		#count OD pairs in TAZ OD for given interval
 		cur.execute("SELECT COUNT(*) \
@@ -71,6 +108,6 @@ if __name__ == '__main__':
 
 		#convert to cell flows
 		mapper = util.MapReduce(calculate_cell_od_flow, upload_cell_od_flow)
-		mapper(fetch_taz_od(), pipe = True, out = False, chunksize = 1000)
+		mapper(fetch_taz_od_chunks(), length = count/CHUNKSIZE, pipe = True, out = False)
 
 
