13ca9c3693834138dbc7e24bb2f07d1d9f37054d
Nils Breyer <nbnet@arcor.de>
2015-06-20 21:14:58 -0700

    speedup network loading with index on cellpath_dist view

--- SQL/04_Routing_Network_Loading/create_cellpath_dist.sql
+++ SQL/04_Routing_Network_Loading/create_cellpath_dist.sql
@@ -11,9 +11,14 @@ CREATE MATERIALIZED VIEW cellpath_dist AS
                                       FROM trips AS trips_all 
                                       WHERE trips_all.start_antenna = trips.start_antenna 
                                         AND trips_all.end_antenna = trips.end_antenna 
-                                        AND array_length(cellpath,1) >= 3) 
+                                        AND array_length(trips_all.cellpath,1) >= 3) 
             AS share --probablity of the cellpath for the given cell od pair
   FROM trips
   WHERE array_length(cellpath,1) >= 3
   GROUP BY trips.start_antenna, trips.end_antenna, cellpath
 WITH DATA;
+
+CREATE INDEX idx_cellpath_dist_orig_dest
+  ON public.cellpath_dist
+  USING btree
+  (orig_cell, dest_cell);
