ca2422653370cee5a64a443e4d284228ab037a03
Nils Breyer <nbnet@arcor.de>
2015-06-22 15:15:30 -0700

    fix voronoi

--- voronoi.py
+++ voronoi.py
@@ -11,6 +11,7 @@ cur.execute(open("SQL/01_Loading/create_voronoi.sql", 'r').read())
 
 print("Creating voronoi() function...")
 cur.execute(open("SQL/01_Loading/create_voronoi_func.sql", 'r').read())
+cur.commit()
 
 print("Calculating Voronoi partition...")
 cur.execute("INSERT INTO voronoi SELECT v.id, v.geom FROM voronoi('ant_pos', 'geom') AS v(id numeric(10,0), geom geometry(Polygon,4326))")
