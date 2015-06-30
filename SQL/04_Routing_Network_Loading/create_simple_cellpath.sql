1ae0c04c0328e8ac1d8779813617d12bdacaf850
Nils Breyer <nbnet@arcor.de>
2015-06-30 15:25:35 -0700

    add functions to fetch cell path segments for lazy Voronoi

--- SQL/04_Routing_Network_Loading/create_simple_cellpath.sql
+++ SQL/04_Routing_Network_Loading/create_simple_cellpath.sql
@@ -24,3 +24,45 @@ CREATE INDEX idx_simple_cellpath_cellpath
   ON public.simple_cellpath
   USING btree
   (cellpath);
+
+
+--_assignCellpathSegmentids(cellpath) assigns segment ids to each cell in the cellpath based on the simplified cellpath
+CREATE OR REPLACE FUNCTION _assignCellpathSegmentIds(integer[]) RETURNS TABLE(segment_id integer, cellid integer) AS
+$BODY$
+  DECLARE
+    simplified integer[];
+    BEGIN
+    simplified := (SELECT simple_cellpath FROM simple_cellpath WHERE simple_cellpath.cellpath = $1);
+    segment_id := 0;
+    FOR i IN 1 .. array_length($1,1) LOOP
+      cellid := $1[i];
+    IF $1[i] = simplified[segment_id+1] AND i < array_length($1,1) THEN
+      IF i > 1 THEN
+        RETURN NEXT;
+      END IF;
+      segment_id := segment_id + 1;
+      RETURN NEXT;
+    ELSE
+      RETURN NEXT;
+    END IF;
+    END LOOP;
+    RETURN;
+    END
+$BODY$
+LANGUAGE 'plpgsql'
+IMMUTABLE
+RETURNS NULL ON NULL INPUT;
+
+
+--getCellpathSegments(cellpath) returns all major segments of a cellpath with segment id and an array of cells in the segment
+CREATE OR REPLACE FUNCTION getCellpathSegments(integer []) RETURNS TABLE(segment_id integer, segment integer[]) AS $$
+  SELECT segment_id, array_agg(cellid) 
+  FROM _assignCellpathSegmentIds($1)
+  GROUP BY segment_id
+  ORDER BY segment_id
+$$ LANGUAGE SQL STABLE;
+
+
+--Testing:
+--SELECT * FROM _assignCellpathSegmentIds(ARRAY[63,60,61,62,222,361,358,223,359,225,230,356,381,351,362,349,362,33,28,350,27,388,0,23,22,17,302,295,303,297,306])
+--SELECT * FROM getCellpathSegments(ARRAY[63,60,61,62,222,361,358,223,359,225,230,356,381,351,362,349,362,33,28,350,27,388,0,23,22,17,302,295,303,297,306])
