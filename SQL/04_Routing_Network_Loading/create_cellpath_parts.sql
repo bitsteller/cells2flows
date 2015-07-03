58318c5ce414999402eb40f4e96f34216c44e52a
Nils Breyer <nbnet@arcor.de>
2015-07-02 21:11:57 -0700

    add simple cellparts for waypoint calculation
    last missing part for lazy voronoi

--- SQL/04_Routing_Network_Loading/create_cellpath_parts.sql
+++ SQL/04_Routing_Network_Loading/create_cellpath_parts.sql
@@ -4,8 +4,17 @@
 DROP MATERIALIZED VIEW IF EXISTS cellpath_parts CASCADE;
 
 CREATE MATERIALIZED VIEW cellpath_parts AS 
-SELECT DISTINCT parts.part AS part
-			FROM trips, getParts(trips.cellpath) AS parts
-			WHERE NOT parts.part[1] = parts.part[2] AND NOT parts.part[3] = parts.part[2]
-      			  AND start_antenna IN %(cells)s AND end_antenna IN %(cells)s
+SELECT DISTINCT * 
+FROM ((	SELECT parts.part AS part
+	  	FROM trips, getParts(trips.cellpath) AS parts
+	  	WHERE NOT parts.part[1] = parts.part[2] AND NOT parts.part[3] = parts.part[2]
+      		AND start_antenna IN %(cells)s AND end_antenna IN %(cells)s
+      )
+	UNION 
+	 (	SELECT parts.part AS part
+	  	FROM simple_cellpath, getParts(simple_cellpath.simple_cellpath) AS parts
+		WHERE NOT parts.part[1] = parts.part[2] AND NOT parts.part[3] = parts.part[2]
+      		AND cellpath[0] IN %(cells)s AND cellpath[array_length(cellpath,1)] IN %(cells)s
+      )
+	 )
 WITH DATA;
