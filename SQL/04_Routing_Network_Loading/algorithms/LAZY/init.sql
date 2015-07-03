62ee237c243fea3eab8758543ebbfe01cfb0f359
Nils Breyer <nbnet@arcor.de>
2015-07-02 17:36:57 -0700

    LAZY voronoi routing helper functions

--- SQL/04_Routing_Network_Loading/algorithms/LAZY/init.sql
+++ SQL/04_Routing_Network_Loading/algorithms/LAZY/init.sql
@@ -24,8 +24,9 @@ WITH DATA;
 
 --routeSegmentLazy(start_junction, end_junction, segment) for an array of cellids (segment) 
 -- calculates a route from start_junction (hh_2po_4pgr_vertices.id) to end_junction preferring links inside the segment cells
-CREATE OR REPLACE FUNCTION routeSegmentLazy(integer, integer, integer[]) RETURNS integer[] AS $$
-	SELECT array_agg(r.id2)
+-- returns a setof linkids (hh_2po_4pgr.id)
+CREATE OR REPLACE FUNCTION routeSegmentLazy(integer, integer, integer[]) RETURNS SETOF integer AS $$
+	SELECT r.id2
  	FROM pgr_dijkstra('	WITH preferred_links AS 
  							(SELECT * FROM public.hh_2po_4pgr_lite_with_cells
  						 	WHERE hh_2po_4pgr_lite_with_cells.cellid = ANY(ARRAY[' || array_to_string($3,',') || ']))
@@ -43,3 +44,25 @@ $$ LANGUAGE SQL STABLE;
 
 --test:
 --SELECT routeSegmentLazy(best_startpoint(ARRAY[0,4]), best_endpoint(ARRAY[8,196]),ARRAY[0,4,0,4,186,4,316,185,215,346,385,357,219,220,331,207,365,8,196])
+
+
+--routeLazy(cellpath) for an array of cellids (cellpath) 
+-- calculates a route roughly following the cellpath (uses characteristic cells as 
+-- waypoints and modified cost on links inside visited cells)
+CREATE OR REPLACE FUNCTION routeLazy(integer[]) RETURNS integer[] AS $$
+	WITH via AS (SELECT array_append(array_prepend(best_startpoint($1), 
+												   get_waypoints(scp.simple_cellpath)), 
+												   best_endpoint($1)) AS points
+				 FROM simple_cellpath AS scp
+				 WHERE scp.cellpath = $1),
+	segment AS (SELECT segment_id, segment 
+				FROM cellpath_segment
+				WHERE cellpath_segment.cellpath = $1
+				ORDER BY segment_id ASC)
+	SELECT array_agg(routeSegmentLazy(via.points[segment_id], via.points[segment_id+1], segment.segment))
+	FROM segment, via;
+$$ LANGUAGE SQL STABLE;
+
+
+
+
