406dd9636aa21caed804bec166542829fb2b8596
Nils Breyer <nbnet@arcor.de>
2015-07-10 17:19:20 -0700

    fix simplify_network

--- SQL/02_Network_Simplification/create_hh_2po_4pgr_vertices.sql
+++ SQL/02_Network_Simplification/create_hh_2po_4pgr_vertices.sql
@@ -29,9 +29,10 @@ CREATE INDEX hh_2po_4pgr_vertices_geom_idx
 --Insert intersections from hh_2po_4pgr
 INSERT INTO hh_2po_4pgr_vertices (id, geom)
 WITH p AS ((SELECT source AS id, x1 AS x, y1 AS y FROM hh_2po_4pgr) UNION (SELECT target AS id, x2 AS x, y2 AS y FROM hh_2po_4pgr))
-SELECT id, ST_SetSRID(ST_Makepoint(x, y),4326) FROM p
+SELECT id, ST_SetSRID(ST_Makepoint(x, y),4326) FROM p;
 
 --also create index for edges
+DROP INDEX IF EXISTS hh_2po_4pgr_geom_way_idx CASCADE;
 CREATE INDEX hh_2po_4pgr_geom_way_idx
   ON hh_2po_4pgr
   USING GIST (geom_way);
