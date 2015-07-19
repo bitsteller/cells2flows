968ba0e08797b195b4d0868d55653e809efd7ab8
Nils Breyer <nbnet@arcor.de>
2015-07-19 16:20:47 -0700

    more tests

--- test_db_integrity_fast.py
+++ test_db_integrity_fast.py
@@ -5,7 +5,8 @@ import config, util
 
 class TestVerifyDBFast(unittest.TestCase):
 	"""Runs several test to verify the integrity of the database. 
-	Some tests only check a portion of the data, please run slow tests additionally to check all data.
+	Caution! Some tests only check a small random selected portion of the data, 
+	please run slow tests additionally to check all data.
 	"""
 	def setUp(self):
 		self.conn = util.db_connect()
@@ -36,7 +37,6 @@ class TestVerifyDBFast(unittest.TestCase):
 		self.assertEqual(self.cur.rowcount, 0)
 
 	def test_road_networks_ok(self):
-		#check full network
 		sql = "	SELECT pgr_createTopology('hh_2po_4pgr', 0.000002, 'geom_way'); \
 				SELECT pgr_analyzeGraph('hh_2po_4pgr', 0.000002, 'geom_way');"
 		self.cur.execute(sql)
@@ -47,7 +47,6 @@ class TestVerifyDBFast(unittest.TestCase):
 		self.assertEqual("OK", result)
 
 	def test_road_network_simplified_ok(self):
-		#check simplified network
 		sql = "	SELECT pgr_createTopology('hh_2po_4pgr_lite', 0.000002, 'geom_way'); \
 				SELECT pgr_analyzeGraph('hh_2po_4pgr_lite', 0.000002, 'geom_way');"
 		self.cur.execute(sql)
@@ -58,7 +57,8 @@ class TestVerifyDBFast(unittest.TestCase):
 		self.assertEqual("OK", result)
 
 	def test_od_convert_flow_conservation(self):
-		"""Checks if no major flows where lost during OD matrix conversion"""
+		"""Checks if no major flows where lost during OD matrix conversion. 
+		Flows can be lost if certain TAZs are not touched by any Voronoi cell."""
 
 		sql = "SELECT (SELECT SUM(flow) FROM taz_od) AS total_taz_od_flow, (SELECT SUM(flow) FROM od) AS total_od_flow"
 		self.cur.execute(sql)
@@ -69,6 +69,30 @@ class TestVerifyDBFast(unittest.TestCase):
 		self.assertTrue(total_od_flow < 1.05*total_taz_od_flow) #allow up to 5% difference
 		self.assertTrue(total_od_flow > 0.95*total_taz_od_flow) #allow up to 5% difference
 
+	def test_all_od_pairs_have_cellpath(self):
+		sql = "SELECT COUNT(*) FROM (SELECT * FROM od ORDER BY random() LIMIT 1000) AS od WHERE NOT EXISTS (SELECT * FROM getTopCellpaths(od.orig_cell, od.dest_cell, 1))"
+		self.cur.execute(sql)
+		od_pairs_with_missing_cellpath = self.cur.fetchone()[0]
+		self.assertEqual(0, od_pairs_with_missing_cellpath)
+
+	def test_all_cellpaths_have_segments(self):
+		sql = "	SELECT COUNT(*) \
+				FROM (SELECT * FROM od ORDER BY random() LIMIT 1000) AS od, LATERAL getTopCellpaths(od.orig_cell, od.dest_cell, 1) cp \
+				WHERE NOT EXISTS(SELECT * FROM cellpath_segment WHERE cellpath_segment.cellpath = cp.cellpath)"
+		self.cur.execute(sql)
+		cellpaths_with_missing_segments = self.cur.fetchone()[0]
+		self.assertEqual(0, cellpaths_with_missing_segments)
+
+	def test_all_waypoints_found(self):
+		sql = "	SELECT COUNT(*) \
+				FROM (SELECT * FROM od ORDER BY random() LIMIT 1000) AS od, LATERAL getTopCellpaths(od.orig_cell, od.dest_cell, 1) cp \
+				WHERE EXISTS(SELECT 1 FROM unnest(get_waypoints(cp.cellpath)) w(p) WHERE p IS NULL) \
+					OR array_length(get_waypoints(cp.cellpath),1) != array_length(cp.cellpath,1) - 2 \
+					OR get_waypoints(cp.cellpath) IS NULL"
+		self.cur.execute(sql)
+		cellpaths_with_missing_waypoints = self.cur.fetchone()[0]
+		self.assertEqual(0, cellpaths_with_missing_waypoints)
+
 	def test_route_lazy_all_flow_assigned(self):
 		pass
 
@@ -77,13 +101,4 @@ class TestVerifyDBFast(unittest.TestCase):
 
 	def test_route_shortest_all_flow_assigned(self):
 		pass
-
-	def test_all_cellpaths_have_segments(self):
-		pass
-
-	def test_all_od_pairs_have_cellpath(self):
-		sql = "SELECT COUNT(*) FROM (SELECT * FROM od ORDER BY random() LIMIT 1000) AS od WHERE NOT EXISTS (SELECT * FROM getTopCellpaths(od.orig_cell, od.dest_cell, 1))"
-		self.cur.execute(sql)
-		od_pairs_with_missing_cellpath = self.cur.fetchone()[0]
-		self.assertEqual(0, od_pairs_with_missing_cellpath)
 
--- test_db_integrity_slow.py
+++ test_db_integrity_slow.py
@@ -19,3 +19,20 @@ class TestVerifyDBSlow(unittest.TestCase):
 		od_pairs_with_missing_cellpath = self.cur.fetchone()[0]
 		self.assertEqual(0, od_pairs_with_missing_cellpath)
 
+	def test_all_cellpaths_have_segments(self):
+		sql = "	SELECT COUNT(*) \
+				FROM od, LATERAL getTopCellpaths(od.orig_cell, od.dest_cell, 1) cp \
+				WHERE NOT EXISTS(SELECT * FROM cellpath_segment WHERE cellpath_segment.cellpath = cp.cellpath)"
+		self.cur.execute(sql)
+		cellpaths_with_missing_segments = self.cur.fetchone()[0]
+		self.assertEqual(0, cellpaths_with_missing_segments)
+
+	def test_all_waypoints_found(self):
+		sql = "	SELECT COUNT(*) \
+				FROM od, LATERAL getTopCellpaths(od.orig_cell, od.dest_cell, 1) cp \
+				WHERE EXISTS(SELECT 1 FROM unnest(get_waypoints(cp.cellpath)) w(p) WHERE p IS NULL) \
+					OR array_length(get_waypoints(cp.cellpath),1) != array_length(cp.cellpath,1) - 2 \
+					OR get_waypoints(cp.cellpath) IS NULL"
+		self.cur.execute(sql)
+		cellpaths_with_missing_waypoints = self.cur.fetchone()[0]
+		self.assertEqual(0, cellpaths_with_missing_waypoints)
