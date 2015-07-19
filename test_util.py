623ccbf0fdde0174a63a227a47f2299357e5e1ce
Nils Breyer <nbnet@arcor.de>
2015-07-19 15:23:58 -0700

    faster tests for util.py

--- test_util.py
+++ test_util.py
@@ -18,22 +18,22 @@ class TestUtil(unittest.TestCase):
 		pass
 
 	def test_map(self):
-		l = [1]*90000 + [2]*5000
+		l = [1]*50000 + [2]*5000
 		random.shuffle(l)
 
 		mapper = util.ParMap(mapf, num_workers = 4)
 		r = mapper(l, chunksize = 100)
-		self.assertEqual(90000, sum([1 for kv in r if kv == [(1,1)]]))
+		self.assertEqual(50000, sum([1 for kv in r if kv == [(1,1)]]))
 		self.assertEqual(5000, sum([1 for kv in r if kv == [(2,1)]]))
 
 	def test_mapreduce(self):
-		l = [1]*90000 + [2]*5000
+		l = [1]*50000 + [2]*5000
 		random.shuffle(l)
 
 		mapper = util.MapReduce(mapf,redf, num_workers = 4)
 		r = mapper(l, chunksize = 100)
 		
-		self.assertEqual(set([(1,90000), (2,5000)]), set(r))
+		self.assertEqual(set([(1,50000), (2,5000)]), set(r))
 
 	def test_chunks(self):
 		l = [1,2,3,4,5,6,7,8,9]
