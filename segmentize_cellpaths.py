022d1058ecf36db87fb7ee53eb6b62a5c9ec0d6d
Nils Breyer <nbnet@arcor.de>
2015-07-21 14:55:08 -0700

    simplification tolerance as config parameter

--- segmentize_cellpaths.py
+++ segmentize_cellpaths.py
@@ -6,8 +6,8 @@ util.db_login()
 conn = util.db_connect()
 cur = conn.cursor()
 
-print("Creating simple_cellpaths view (takes a while)...")
-cur.execute(open("SQL/04_Routing_Network_Loading/create_simple_cellpath.sql", 'r').read())
+print("Creating simple_cellpath view (takes a while)...")
+cur.execute(open("SQL/04_Routing_Network_Loading/create_simple_cellpath.sql", 'r').read(), {"tolerance": config.SIMPLIFICATION_TOLERANCE})
 conn.commit()
 
 print("Creating cellpath_segment view (takes a while)...")
