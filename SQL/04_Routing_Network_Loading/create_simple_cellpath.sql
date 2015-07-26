d2f90d821d3ca06fbe030f81e50273fd7953e47e
Nils Breyer <nbnet@arcor.de>
2015-07-26 15:35:41 +0200

    fix: make sure simple cell paths have at least 2 cells

--- SQL/04_Routing_Network_Loading/create_simple_cellpath.sql
+++ SQL/04_Routing_Network_Loading/create_simple_cellpath.sql
@@ -3,7 +3,7 @@ DROP MATERIALIZED VIEW IF EXISTS simple_cellpath CASCADE;
 
 CREATE MATERIALIZED VIEW simple_cellpath AS 
   WITH cellpath_geom AS
-  (WITH cellpaths AS (SELECT DISTINCT cellpath FROM cellpath_dist)
+  (WITH cellpaths AS (SELECT DISTINCT cellpath FROM cellpath_dist LIMIT 1000)
   SELECT cellpaths.cellpath,
       ( SELECT st_makeline(( SELECT ant_pos.geom
                      FROM ant_pos
@@ -11,13 +11,21 @@ CREATE MATERIALIZED VIEW simple_cellpath AS
              FROM unnest(cellpaths.cellpath) cellid(cellid)) AS geom,
      ( SELECT st_simplify(st_makeline(( SELECT ant_pos.geom
              FROM ant_pos
-            WHERE ant_pos.id = cellid.cellid)),%(tolerance)s) AS st_simplify
+            WHERE ant_pos.id = cellid.cellid)),0.03) AS st_simplify
      FROM unnest(cellpaths.cellpath) cellid(cellid)) AS simple_geom
      FROM cellpaths)
   SELECT  cellpath_geom.cellpath, 
-          (SELECT array_agg(ant_pos.id) 
+
+          (SELECT CASE WHEN array_length(scp.cells,1) = 1 THEN
+                  ARRAY[scp.cells[1],scp.cells[1]]
+                ELSE
+                  scp.cells
+                 END
+           FROM (SELECT array_agg(ant_pos.id) AS cells
            FROM ST_DumpPoints(cellpath_geom.simple_geom) scpg
-           JOIN ant_pos ON ant_pos.geom = scpg.geom) AS simple_cellpath
+           JOIN ant_pos ON ant_pos.geom = scpg.geom) AS scp)
+
+          AS simple_cellpath
   FROM cellpath_geom
   GROUP BY cellpath_geom.cellpath, cellpath_geom.simple_geom
 WITH DATA;
