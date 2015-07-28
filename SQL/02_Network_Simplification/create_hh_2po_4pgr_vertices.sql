44467da05ab6a9148dd70dc39aa3fd4fa60db9ee
Nils Breyer <nbnet@arcor.de>
2015-07-28 19:49:34 +0200

    cleanup

--- SQL/02_Network_Simplification/create_hh_2po_4pgr_vertices.sql
+++ SQL/02_Network_Simplification/create_hh_2po_4pgr_vertices.sql
@@ -3,17 +3,9 @@ DROP INDEX IF EXISTS hh_2po_4pgr_vertices_geom_idx;
 DROP TABLE IF EXISTS public.hh_2po_4pgr_vertices CASCADE;
 DROP SEQUENCE IF EXISTS public.hh_2po_4pgr_vertices_id_seq;
 
---Create ant_pos table and index
-CREATE SEQUENCE public.hh_2po_4pgr_vertices_id_seq
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-
 CREATE TABLE public.hh_2po_4pgr_vertices
 (
-  id integer NOT NULL DEFAULT nextval('hh_2po_4pgr_vertices_id_seq'::regclass),
+  id integer NOT NULL,
   geom geometry(Geometry,4326),
   CONSTRAINT hh_2po_4pgr_vertices_pkey PRIMARY KEY (id)
 )
