15d2df3767418fcaaf3497dc697ae021a2373e04
Nils Breyer <nbnet@arcor.de>
2015-07-31 15:24:48 +0200

    fix: copy&paste error in shortest algo

--- SQL/04_Routing_Network_Loading/algorithms/SHORTEST/init.sql
+++ SQL/04_Routing_Network_Loading/algorithms/SHORTEST/init.sql
@@ -1,7 +1,7 @@
 --routeShortest(cellpath) for an array of cellids (cellpath) 
 -- calculates a route following the shortest path between the first and last cell in the cellpath
 -- returns an array of hh_2po_4pgr link ids
-CREATE OR REPLACE FUNCTION routeStrict(integer[]) RETURNS integer[] AS $$
+CREATE OR REPLACE FUNCTION routeShortest(integer[]) RETURNS integer[] AS $$
 	SELECT (route_with_waypoints(array_append(array_prepend(best_startpoint($1), 
 														ARRAY[]::int[]), 
 														best_endpoint($1)))).edges AS links;
@@ -9,5 +9,5 @@ $$ LANGUAGE SQL STABLE;
 
 --make shortest the default routing algorithm
 CREATE OR REPLACE FUNCTION route(integer[]) RETURNS integer[] AS $$
-	SELECT routeStrict($1) AS links;
+	SELECT routeShortest($1) AS links;
 $$ LANGUAGE SQL STABLE;
