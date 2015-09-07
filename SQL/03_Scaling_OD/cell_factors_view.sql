507232309511880b9e25a867bc437885ffc225ea
Nils Breyer <nbnet@arcor.de>
2015-09-07 15:57:00 +0200

    fix cell_factors

--- SQL/03_Scaling_OD/cell_factors_view.sql
+++ SQL/03_Scaling_OD/cell_factors_view.sql
@@ -3,7 +3,7 @@ CREATE MATERIALIZED VIEW cell_factors AS
 (
  WITH days AS (SELECT generate_series(MIN(start_time), MAX(start_time), '1 day'::interval) AS day FROM trips)
  SELECT homebase.antenna_id,
-    day.day,
+    days.day,
     count(DISTINCT trips.user_id) AS active_users,
     user_home_count.no_users,
     count(DISTINCT trips.user_id)::double precision / user_home_count.no_users::double precision AS active_share,
@@ -14,6 +14,6 @@ CREATE MATERIALIZED VIEW cell_factors AS
     days,
     cell_population
   WHERE homebase.antenna_id = ANY(%(cells)s) AND homebase.user_id = trips.user_id AND homebase.antenna_id = user_home_count.antenna_id AND trips.start_time >= days.day AND trips.start_time <= (days.day + '1 day'::interval) AND cell_population.antenna_id = homebase.antenna_id
-  GROUP BY homebase.antenna_id, user_home_count.no_users, day.day, cell_population.population
-  ORDER BY homebase.antenna_id, day.day
+  GROUP BY homebase.antenna_id, user_home_count.no_users, days.day, cell_population.population
+  ORDER BY homebase.antenna_id, days.day
 ) WITH DATA;
